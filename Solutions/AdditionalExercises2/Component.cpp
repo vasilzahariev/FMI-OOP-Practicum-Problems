7e40707bdb1d22683bcb396ebfd9a75dd7429751
Vasil Zahariev <vasilzahariev1707@abv.bg>
2022-05-23 17:45:13 +0300

    fix: Oops

--- Solutions/AdditionalExercises2/Component.cpp
+++
@@ -1,9 +0,0 @@
-#include "Component.h"
-
-void Component::output(std::ostream& out) const {
-	out << m_label << " " << price() << '\n';
-}
-
-Component::Component(std::string_view label)
-				: m_label(label) {
-}
